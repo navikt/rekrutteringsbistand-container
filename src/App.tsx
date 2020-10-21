fa3f3944bdc98f0bdb69330f548de57c5f1ab499
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-21 13:39:11 +0200

    Legg til tooling og mer

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+import './App.less';
+
+const App: FunctionComponent = () => (
+    <div className="App">
+        <header className="App-header">
+            <h1>Rekrutteringsbistand-container</h1>
+        </header>
+    </div>
+);
+
+export default App;

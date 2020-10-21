18f3c115df6f03d075c427f3561243f7523152c3
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-21 14:39:07 +0200

    Fiks URL til js- og css-filer

--- server/server.js
+++ server/server.js
@@ -11,8 +11,9 @@ const startServer = () => {
         res.sendStatus(200)
     );
 
-    app.use(`/${basePath}/static/js`, express.static(`${buildPath}/static/js`));
-    app.use(`/${basePath}/static/css`, express.static(`${buildPath}/static/css`));
+    app.use(`${basePath}/static/js`, express.static(`${buildPath}/static/js`));
+    app.use(`${basePath}/static/css`, express.static(`${buildPath}/static/css`));
+
     app.get([`${basePath}/`, `${basePath}/*`], (_, res) => {
         res.sendFile(`${buildPath}/index.html`);
     });

6b92f66d4cd7f33532886b79a52a2b9657bc0c34
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-21 15:41:36 +0200

    Importer statistikk-app og render med navspa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React, { FunctionComponent } from 'react';
+import Navspa from '@navikt/navspa';
 import './App.less';
+
+const Statistikk = Navspa.importer('rekrutteringsbistand-statistikk');
 
 const App: FunctionComponent = () => (
     <div className="App">
         <header className="App-header">
             <h1>Rekrutteringsbistand-container</h1>
         </header>
+        <main>
+            <Statistikk />
+        </main>
     </div>
 );
 

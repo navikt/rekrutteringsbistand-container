152070e7fe967bc90f1e85aeebcef2ac4329584f
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2020-10-22 09:44:17 +0200

    Integrer med statistikk (#2)
    * Importer statistikk-app og render med navspa
    
    * Deploy til dev
    
    * Fjern domene fra URL til statistikk
    
    * Importer mocket statistikk-app under utvikling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React, { FunctionComponent } from 'react';
+import Navspa from '@navikt/navspa';
+import MocketStatistikk from './utvikling/MocketStatistikk';
 import './App.less';
+
+const Statistikk =
+    window.location.hostname === 'localhost'
+        ? MocketStatistikk
+        : Navspa.importer('rekrutteringsbistand-statistikk');
 
 const App: FunctionComponent = () => (
     <div className="App">
         <header className="App-header">
             <h1>Rekrutteringsbistand-container</h1>
         </header>
+        <main>
+            <Statistikk />
+        </main>
     </div>
 );
 

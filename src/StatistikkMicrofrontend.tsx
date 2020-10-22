9427b1f475a34231691f499a8229b28d23d95fb9
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-22 11:03:30 +0200

    Kopier async-navspa-implementasjon fra FO

---
+++ src/StatistikkMicrofrontend.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Microfrontend } from './Microfrontend';
+
+export const StatistikkMicrofrontend = () => {
+    return (
+        <Microfrontend
+            applicationName="rekrutteringsbistand-statistikk"
+            applicationBaseUrl="/statistikk"
+        />
+    );
+};

7474a258f90ed4530281f963a4be7fba5271855a
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-23 08:39:49 +0200

    Prøv å fjerne hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React from 'react';
 import { Microfrontend } from './Microfrontend';
 
 const MockChildApp = ({
@@ -18,32 +18,48 @@ const MockChildApp = ({
 
 const ChildApp = window.location.hostname === 'localhost' ? MockChildApp : Microfrontend;
 
-const App: FunctionComponent = () => {
-    const [visning, setVisning] = useState<number>(1);
+class App extends React.Component {
+    state: {
+        visning: number;
+    };
 
-    return (
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            visning: 1,
+        };
+    }
+
+    setVisning = (visning: number) => {
+        this.setState({
+            visning,
+        });
+    };
+
+    render = () => (
         <div className="App">
             <header className="App-header">
                 <h1>Rekrutteringsbistand-container</h1>
             </header>
             <nav>
-                <button onClick={() => setVisning(1)}>Statistikk</button>
-                <button onClick={() => setVisning(2)}>Kandidat</button>
+                <button onClick={() => this.setVisning(1)}>Statistikk</button>
+                <button onClick={() => this.setVisning(2)}>Kandidat</button>
             </nav>
             <main>
                 <ChildApp
                     applicationName="rekrutteringsbistand-statistikk"
                     applicationBaseUrl="/statistikk"
-                    vis={visning === 1}
+                    vis={this.state.visning === 1}
                 />
                 <ChildApp
                     applicationName="rekrutteringsbistand-kandidat"
                     applicationBaseUrl="/kandidater"
-                    vis={visning === 2}
+                    vis={this.state.visning === 2}
                 />
             </main>
         </div>
     );
-};
+}
 
 export default App;

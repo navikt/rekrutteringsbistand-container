ed123087bbe84db4ff98e5e1f24e08bdfdc884c5
Joar Aurdal <joar.aurdal@nav.no>
2020-10-23 10:14:11 +0200

    Legg inn midlertidig mockdeploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Microfrontend } from './Microfrontend';
 
 const MockChildApp = ({
@@ -33,29 +34,34 @@ class App extends React.Component {
         });
     };
 
-    render = () => (
-        <div className="App">
-            <header className="App-header">
-                <h1>Rekrutteringsbistand-container</h1>
-            </header>
-            <nav>
-                <button onClick={() => this.setVisning(1)}>Statistikk</button>
-                <button onClick={() => this.setVisning(2)}>Kandidat</button>
-            </nav>
-            <main>
-                <ChildApp
-                    applicationName="rekrutteringsbistand-statistikk"
-                    applicationBaseUrl="/statistikk"
-                    vis={this.state.visning === 1}
-                />
-                <ChildApp
-                    applicationName="rekrutteringsbistand-kandidat"
-                    applicationBaseUrl="/kandidater"
-                    vis={this.state.visning === 2}
-                />
-            </main>
-        </div>
-    );
+    componentDidUpdate() {
+        if (this.state.visning === 1) {
+            ReactDOM.render(
+                <div>Rekrutteringsbistand-statistikk2</div>,
+                document.getElementById('main')
+            );
+        } else {
+            ReactDOM.render(
+                <div>Rekrutteringsbistand-kandidat2</div>,
+                document.getElementById('main')
+            );
+        }
+    }
+
+    render = () => {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Rekrutteringsbistand-container</h1>
+                </header>
+                <nav>
+                    <button onClick={() => this.setVisning(1)}>Statistikk</button>
+                    <button onClick={() => this.setVisning(2)}>Kandidat</button>
+                </nav>
+                <main id="main"></main>
+            </div>
+        );
+    };
 }
 
 export default App;

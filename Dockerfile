f235a1d67365e4387174a3eb4c3ee33d515d2ddd
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-11-04 08:37:20 +0100

    Redirect til loginservice hvis uinnlogget

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,8 @@ WORKDIR /app
 COPY build/ build/
 COPY server/ server/
 
+WORKDIR /app/server
+RUN npm ci
+
 EXPOSE 8080
-ENTRYPOINT ["node", "server/server.js"]
+ENTRYPOINT ["node", "server.js"]
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const express = require('express');
+const jws = require('jws');
 const app = express();
 
 const port = process.env.PORT || 8080;
+const loginserviceUrl = process.env.LOGINSERVICE_URL;
+
 const buildPath = path.join(__dirname, '../build');
 
 const startServer = () => {
@@ -10,16 +13,51 @@ const startServer = () => {
         res.sendStatus(200)
     );
 
+    const pathsForServingApp = ['/', '/*'];
+
     app.use('/static/js', express.static(`${buildPath}/static/js`));
     app.use('/static/css', express.static(`${buildPath}/static/css`));
 
-    app.get(['/', '/*'], (_, res) => {
+    app.get(pathsForServingApp, ensureLoggedIn, (_, res) => {
         res.sendFile(`${buildPath}/index.html`);
     });
 
     app.listen(port, () => {
         console.log('Server kjører på port', port);
     });
+};
+
+const hentLoginTokenFraCookies = (cookies) => {
+    if (cookies !== undefined) {
+        const erLoginCookie = (cookie) => typeof cookie === 'string' && cookie.includes('-idtoken');
+        const loginCookie = cookies.split(';').find(erLoginCookie);
+
+        if (loginCookie) {
+            return loginCookie.split('=').pop().trim();
+        }
+    }
+
+    return undefined;
+};
+
+const tokenErUtløpt = (token) => {
+    let expiration = jws.decode(token).payload.exp;
+    if (expiration.toString().length === 10) {
+        expiration = expiration * 1000;
+    }
+
+    return expiration - Date.now() < 0;
+};
+
+const ensureLoggedIn = (req, res, next) => {
+    const loginToken = hentLoginTokenFraCookies(req.headers.cookie);
+    if (loginToken === undefined || tokenErUtløpt(loginToken)) {
+        const hostname = req.get('host');
+        const redirectUrl = `${loginserviceUrl}?redirect=https://${hostname}`;
+        return res.redirect(redirectUrl);
+    }
+
+    return next();
 };
 
 startServer();

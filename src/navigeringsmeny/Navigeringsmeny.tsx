b32578e24dce20a324790d5e797d9cacbd46db4e
André Mossige <mossige.a@gmail.com>
2020-11-13 11:57:57 +0100

    Fiks aktiv tab visning i navigeringsmeny

--- src/navigeringsmeny/Navigeringsmeny.tsx
+++ src/navigeringsmeny/Navigeringsmeny.tsx
@@ -51,7 +51,7 @@ const Navigeringsmeny: FunctionComponent = () => {
                         <Tab
                             key={tab.href}
                             config={tab}
-                            erAktiv={pathname.startsWith(tab.href)}
+                            erAktiv={pathname === tab.href}
                             onClick={onTabClick(tab.href)}
                         />
                     ))}

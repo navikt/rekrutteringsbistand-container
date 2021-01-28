def0d087c58e62e67f94c8c8981426654373ef0f
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2021-01-28 07:54:54 +0100

    Importer og render Microfrontends med navspa (#28)
    * Importer Microfrontends med ny Navspa-versjon
    
    * Fiks og refaktor importering med Navspa
    
    * Fiks statistikk-app og modia-dekoratør sin URL
    
    * Fiks url for modia-dekoratør
    
    * Fiks url igjen
    
    * Fiks feil i asset manifest-parseren
    
    * Implementer forenklet, gammel Microfrontend-løsning for Modia
    
    * Preload alle microfrontends
    
    * Refaktorer innlasting av Modia-dekoratør
    
    * Refaktor Modia-dekoratør
    
    * Legg tilbake exact path for statistikk-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import React, { FunctionComponent, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { History } from 'history';
 
 import history from './history';
-import ImportertMicrofrontend from './microfrontend/Microfrontend';
-import MocketMicrofrontend from './microfrontend/mock/MocketMicrofrontend';
-import Modiadekoratør from './modia/Modiadekoratør';
 import Navigeringsmeny from './navigeringsmeny/Navigeringsmeny';
-
-const nodeEnvProduction = process.env.NODE_ENV === 'production';
-
-const importerMicrofrontends = process.env.REACT_APP_IMPORT || nodeEnvProduction;
-
-const Microfrontend = importerMicrofrontends ? ImportertMicrofrontend : MocketMicrofrontend;
-
-type FellesMicrofrontendProps = {
-    navKontor: string | null;
-    history: History;
-};
+import Modiadekoratør from './modia/Modiadekoratør';
+import { Stilling, Kandidat, Statistikk, Stillingssøk } from './microfrontends/microfrontends';
 
 const App: FunctionComponent = () => {
     const [navKontor, setNavKontor] = useState<string | null>(null);
@@ -31,48 +18,16 @@ const App: FunctionComponent = () => {
             <main>
                 <Switch>
                     <Route path="/stillinger">
-                        <Microfrontend<FellesMicrofrontendProps>
-                            key="rekrutteringsbistand-stilling"
-                            appName="rekrutteringsbistand-stilling"
-                            appPath="/rekrutteringsbistand-stilling"
-                            appProps={{
-                                navKontor,
-                                history,
-                            }}
-                        />
+                        <Stilling navKontor={navKontor} history={history} />
                     </Route>
                     <Route path="/stillingssok">
-                        <Microfrontend<FellesMicrofrontendProps>
-                            key="rekrutteringsbistand-stillingssok"
-                            appName="rekrutteringsbistand-stillingssok"
-                            appPath="/rekrutteringsbistand-stillingssok"
-                            appProps={{
-                                navKontor,
-                                history,
-                            }}
-                        />
+                        <Stillingssøk navKontor={navKontor} history={history} />
                     </Route>
                     <Route path="/kandidater">
-                        <Microfrontend<FellesMicrofrontendProps>
-                            key="rekrutteringsbistand-kandidat"
-                            appName="rekrutteringsbistand-kandidat"
-                            appPath="/rekrutteringsbistand-kandidat"
-                            appProps={{
-                                navKontor,
-                                history,
-                            }}
-                        />
+                        <Kandidat navKontor={navKontor} history={history} />
                     </Route>
                     <Route exact path="/">
-                        <Microfrontend<FellesMicrofrontendProps>
-                            key="rekrutteringsbistand-statistikk"
-                            appName="rekrutteringsbistand-statistikk"
-                            appPath="/rekrutteringsbistand-statistikk"
-                            appProps={{
-                                navKontor,
-                                history,
-                            }}
-                        />
+                        <Statistikk navKontor={navKontor} history={history} />
                     </Route>
                 </Switch>
             </main>
--- src/modia/Modiadekoratør.tsx
+++ src/modia/Modiadekoratør.tsx
@@ -1,46 +1,85 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import Navspa from '@navikt/navspa';
+import loadjs from 'loadjs';
 import DekoratørProps, { EnhetDisplay } from './DekoratørProps';
-import Microfrontend from '../microfrontend/Microfrontend';
 import './Modiadekoratør.less';
 
-const urlPrefix =
-    process.env.NODE_ENV === 'production'
-        ? 'https://internarbeidsflatedecorator.nais.adeo.no'
-        : 'https://navikt.github.io';
+const appName = 'internarbeidsflatefs';
 
-const erMocket = process.env.REACT_APP_MOCK;
+const hentAssets = () => {
+    const urlPrefix =
+        process.env.NODE_ENV === 'production'
+            ? 'https://internarbeidsflatedecorator.nais.adeo.no'
+            : 'https://navikt.github.io';
+
+    return [
+        `${urlPrefix}/internarbeidsflatedecorator/v2.1/static/js/head.v2.min.js`,
+        `${urlPrefix}/internarbeidsflatedecorator/v2.1/static/css/main.css`,
+    ];
+};
+
+enum Status {
+    LasterNed,
+    Klar,
+    Feil,
+}
 
 type Props = {
     navKontor: string | null;
     onNavKontorChange: (navKontor: string) => void;
 };
 
-const Placeholder = () => <div className="modiadekoratør__placeholder" />;
+const Modiadekoratør: FunctionComponent<Props> = ({ navKontor, onNavKontorChange }) => {
+    const Microfrontend = Navspa.importer<DekoratørProps>(appName);
 
-const Modiadekoratør: FunctionComponent<Props> = ({ navKontor, onNavKontorChange }) => (
-    <div className={`modiadekoratør${erMocket ? ' modiadekoratør--mocket' : ''}`}>
-        <Microfrontend<DekoratørProps>
-            brukNavspa
-            appName="internarbeidsflatefs"
-            placeholder={<Placeholder />}
-            staticPaths={[
-                `${urlPrefix}/internarbeidsflatedecorator/v2.1/static/js/head.v2.min.js`,
-                `${urlPrefix}/internarbeidsflatedecorator/v2.1/static/css/main.css`,
-            ]}
-            appProps={{
-                appname: 'Rekrutteringsbistand',
-                enhet: {
-                    initialValue: navKontor,
-                    display: EnhetDisplay.ENHET_VALG,
-                    onChange: onNavKontorChange,
-                    ignoreWsEvents: true,
-                },
-                toggles: {
-                    visVeileder: true,
-                },
-            }}
-        />
-    </div>
-);
+    const [status, setStatus] = useState<Status>(
+        loadjs.isDefined(appName) ? Status.Klar : Status.LasterNed
+    );
+
+    useEffect(() => {
+        const loadAssets = async (staticPaths: string[]) => {
+            try {
+                await loadjs(staticPaths, appName, {
+                    returnPromise: true,
+                });
+
+                setStatus(Status.Klar);
+            } catch (e) {
+                setStatus(Status.Feil);
+            }
+        };
+
+        if (!loadjs.isDefined(appName)) {
+            loadAssets(hentAssets());
+        }
+    }, []);
+
+    const className = `modiadekoratør${
+        process.env.REACT_APP_MOCK ? ' modiadekoratør--mocket' : ''
+    }`;
+
+    if (status === Status.LasterNed) {
+        return <div className="modiadekoratør__placeholder" />;
+    } else if (status === Status.Klar) {
+        return (
+            <div className={className}>
+                <Microfrontend
+                    appname="Rekrutteringsbistand"
+                    enhet={{
+                        initialValue: navKontor,
+                        display: EnhetDisplay.ENHET_VALG,
+                        onChange: onNavKontorChange,
+                        ignoreWsEvents: true,
+                    }}
+                    toggles={{
+                        visVeileder: true,
+                    }}
+                />
+            </div>
+        );
+    } else {
+        return <span>Klarte ikke å laste inn Modia-dekoratør</span>;
+    }
+};
 
 export default Modiadekoratør;

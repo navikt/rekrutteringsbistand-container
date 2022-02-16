4d6a7967c8913448f18b370c50531048a59a5a59
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-02-16 09:54:39 +0100

    Typesett serveren

--- server/src/authorization.ts
+++ server/src/authorization.ts
@@ -1,19 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import { IncomingHttpHeaders } from 'http';
+
+type Middleware = (req: Request, res: Response, next: NextFunction) => void;
+
 const cluster = process.env.NAIS_CLUSTER_NAME;
 
-const retrieveToken = (headers) => headers.authorization?.replace('Bearer ', '');
+const retrieveToken = (headers: IncomingHttpHeaders) =>
+    headers.authorization?.replace('Bearer ', '');
 
-const tokenIsValid = (token) => {
+const tokenIsValid = (token: string) => {
     return true;
 };
 
-const userIsLoggedIn = (req) => {
+const userIsLoggedIn = (req: Request) => {
     const token = retrieveToken(req.headers);
     console.log('Authorization header er definert:', !!token);
 
     return token && tokenIsValid(token);
 };
 
-export const ensureLoggedIn = (req, res, next) => {
+export const ensureLoggedIn: Middleware = (req, res, next) => {
     if (userIsLoggedIn(req)) {
         next();
     } else {
@@ -21,7 +27,7 @@ export const ensureLoggedIn = (req, res, next) => {
     }
 };
 
-export const opprettCookieFraAuthorizationHeader = (req, res, next) => {
+export const opprettCookieFraAuthorizationHeader: Middleware = (req, res, next) => {
     const token = retrieveToken(req.headers);
 
     if (token) {

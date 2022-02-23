a7284c7844990d206e510412a904617e11a18c9a
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-02-23 08:42:26 +0100

    Bruk container-app som proxy for API-kall

---
+++ server/src/middlewares.ts
@@ -0,0 +1,70 @@
+import { NextFunction, Request, Response } from 'express';
+import { IncomingHttpHeaders } from 'http';
+import { tokenIsValid } from './azureAd';
+import { hentOnBehalfOfToken } from './onBehalfOfToken';
+
+type Middleware = (req: Request, res: Response, next: NextFunction) => void;
+
+const cluster = process.env.NAIS_CLUSTER_NAME;
+
+export const ensureLoggedIn: Middleware = async (req, res, next) => {
+    if (await userIsLoggedIn(req)) {
+        next();
+    } else {
+        res.redirect(`/oauth2/login?redirect=${req.originalUrl}`);
+    }
+};
+
+export const opprettCookieFraAuthorizationHeader: Middleware = (req, res, next) => {
+    const token = retrieveToken(req.headers);
+
+    if (token) {
+        const cookieDomain = cluster === 'prod-gcp' ? 'intern.nav.no' : 'dev.intern.nav.no';
+
+        res.header(
+            'Set-Cookie',
+            `isso-idtoken=${token}; Domain=${cookieDomain}; Secure; HttpOnly; SameSite=Lax;`
+        );
+
+        next();
+    } else {
+        res.status(500).send('Klarte ikke å opprette isso-idtoken-cookie');
+    }
+};
+
+export const removeIssoIdToken: Middleware = async (req, _, next) => {
+    console.log('Mottok cookies:', req.cookies);
+    req.cookies['isso-idtoken'] = undefined;
+    next();
+};
+
+export const setOnBehalfOfToken = (scope: string) => async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const accessToken = retrieveToken(req.headers);
+
+    if (!accessToken) {
+        res.status(500).send('Kan ikke be om OBO-token siden access-token ikke finnes');
+    } else {
+        try {
+            console.log('Henter on behalf of-token for scope', scope);
+
+            const token = await hentOnBehalfOfToken(accessToken, scope);
+            req.headers.authorization = `Bearer ${token.access_token}`;
+            next();
+        } catch (e) {
+            res.status(500).send('Feil ved henting av OBO-token: ' + e);
+        }
+    }
+};
+
+function retrieveToken(headers: IncomingHttpHeaders) {
+    return headers.authorization?.replace('Bearer ', '');
+}
+
+async function userIsLoggedIn(req: Request): Promise<boolean> {
+    const token = retrieveToken(req.headers);
+    return token && (await tokenIsValid(token));
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,24 @@
 import path from 'path';
 import express from 'express';
-import { ensureLoggedIn, opprettCookieFraAuthorizationHeader } from './authorization';
 import { initializeAzureAd } from './azureAd';
+import {
+    ensureLoggedIn,
+    opprettCookieFraAuthorizationHeader,
+    removeIssoIdToken,
+    setOnBehalfOfToken,
+} from './middlewares';
+import { setupProxy } from './proxy';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 const buildPath = path.join(__dirname, '../build');
+
+const cluster = process.env.NAIS_CLUSTER_NAME;
+const fssMiljø = cluster === 'prod-gcp' ? 'prod-fss' : 'dev-fss';
+
+const statistikkApiUrl = process.env.STATISTIKK_API_URL;
+const statistikkApiScope = `api://${fssMiljø}.arbeidsgiver.rekrutteringsbistand-statistikk-api/.default`;
 
 const startServer = () => {
     app.get([`/internal/isAlive`, `/internal/isReady`], (_, res) => res.sendStatus(200));
@@ -15,6 +27,14 @@ const startServer = () => {
 
     app.use('/static/js', express.static(`${buildPath}/static/js`));
     app.use('/static/css', express.static(`${buildPath}/static/css`));
+
+    app.use(
+        `/statistikk-api`,
+        removeIssoIdToken,
+        ensureLoggedIn,
+        setOnBehalfOfToken(statistikkApiScope),
+        setupProxy(`/statistikk-api`, statistikkApiUrl)
+    );
 
     app.get(pathsForServingApp, ensureLoggedIn, opprettCookieFraAuthorizationHeader, (_, res) => {
         res.sendFile(`${buildPath}/index.html`);

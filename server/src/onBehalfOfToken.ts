d0e474958bb695e46708560c9366c86a3c19ab1b
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-02-23 10:37:57 +0100

    Forenkle henting av tokencache

--- server/src/onBehalfOfToken.ts
+++ server/src/onBehalfOfToken.ts
@@ -18,19 +18,16 @@ type AccessToken = string;
 const tokenCache: Record<Scope, Record<AccessToken, CachetOboToken>> = {};
 
 export async function hentOnBehalfOfToken(accessToken: string, scope: string) {
-    const oboTokenFraCache = tokenCache[scope]?.[accessToken];
+    const cacheForScope = hentCacheForScope(scope);
+    const cachetOboToken = cacheForScope[accessToken];
 
-    if (oboTokenFraCache && tokenErFremdelesGyldig(oboTokenFraCache)) {
+    if (cachetOboToken && tokenErFremdelesGyldig(cachetOboToken)) {
         console.log(`Bruker cachet OBO-token for scope ${scope}`);
-        return oboTokenFraCache.token;
+        return cachetOboToken.token;
     } else {
         console.log(`Henter nytt OBO-token for scope ${scope}`);
         const nyttOboToken = await hentNyttOnBehalfOfToken(accessToken, scope);
         const expires = Date.now() + nyttOboToken.expires_in * 1000;
-
-        if (!tokenCache[scope]) {
-            tokenCache[scope] = {};
-        }
 
         tokenCache[scope][accessToken] = {
             token: nyttOboToken,
@@ -40,11 +37,20 @@ export async function hentOnBehalfOfToken(accessToken: string, scope: string) {
         console.log(
             `Cache har nå ${Object.keys(tokenCache).length} scopes, scope "${scope}" har ${
                 Object.keys(tokenCache[scope]).length
-            } entries`
+            } entries:`,
+            Object.keys(tokenCache[scope]).map((accessToken) => accessToken.substring(0, 5))
         );
 
         return nyttOboToken;
     }
+}
+
+function hentCacheForScope(scope: string): Record<AccessToken, CachetOboToken> {
+    if (tokenCache[scope] === undefined) {
+        tokenCache[scope] = {};
+    }
+
+    return tokenCache[scope];
 }
 
 async function hentNyttOnBehalfOfToken(accessToken: string, scope: string): Promise<OboToken> {

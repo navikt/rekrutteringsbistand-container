94d4b9e670c151912e5c454cfb2002202d8ba431
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-06-22 15:18:19 +0200

    Bruk navIdent-claim fra access token for å bestemme featureToggle

--- server/src/featureToggle.ts
+++ server/src/featureToggle.ts
@@ -2,20 +2,18 @@ import { decodeJwt } from 'jose';
 import { Middleware, retrieveToken } from './middlewares';
 
 const autoriserteBrukereForKandidatmatch = process.env.KANDIDATMATCH_AUTORISERTE_BRUKERE || [];
+const navIdentClaim = 'NAVident';
 
 export const featureToggleForKandidatmatch: Middleware = (req, res, next) => {
     // TODO: Tillat alle i cluster:dev-gcp
     const brukerensAccessToken = retrieveToken(req.headers);
     const claims = decodeJwt(brukerensAccessToken);
-
-    console.log('Access token:', featureToggleForKandidatmatch);
-    console.log('Claims:', claims);
-
-    const navIdent = 'A12345';
+    const navIdent = String(claims[navIdentClaim]) || '';
 
     if (autoriserteBrukereForKandidatmatch.includes(navIdent)) {
+        console.log(`Bruker "${navIdent}" bruker kandidatmatch`);
         next();
     } else {
-        res.status(403).send('Brukeren er ikke autorisert til å bruke kandidatmatch');
+        res.status(403).send(`Bruker "${navIdent}" er ikke autorisert til å bruke kandidatmatch`);
     }
 };

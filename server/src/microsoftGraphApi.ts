8ae7e64e72ba1bfa5a1c30890bacfd9e1d0fed29
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-01-25 08:30:38 +0100

    Sjekk tilgang til kandidatsøket med ID-en til AD-rollene
    Ikke displayName slik vi gjorde før

--- server/src/microsoftGraphApi.ts
+++ server/src/microsoftGraphApi.ts
@@ -2,34 +2,32 @@ import fetch from 'node-fetch';
 import { hentOnBehalfOfToken } from './onBehalfOfToken';
 import { logger } from './logger';
 
-const apiScope = "https://graph.microsoft.com/.default";
-const memberOfApiUrl = "https://graph.microsoft.com/v1.0/me/memberOf/?$count=true&$orderby=displayName&$filter=startswith(displayName, '0000-GA')";
-
-export enum AdGruppe {
-    ModiaGenerellTilgang = '0000-GA-BD06_ModiaGenerellTilgang',
-    ModiaOppfølging = '0000-GA-Modia-Oppfolging',
-}
+const apiScope = 'https://graph.microsoft.com/.default';
+const memberOfApiQuery =
+    "$count=true&$orderby=displayName&$filter=startswith(displayName, '0000-GA')";
+const memberOfApiUrl = 'https://graph.microsoft.com/v1.0/me/memberOf/?' + memberOfApiQuery;
 
 type Membership = {
-    displayName: AdGruppe;
+    id: string;
+    displayName: string;
 };
 
 type MemberOfResponse = {
     value: Membership[];
 };
 
-export const hentBrukerensAdGrupper = async (accessToken: string): Promise<AdGruppe[]> => {
+export const hentBrukerensAdGrupper = async (accessToken: string): Promise<string[]> => {
     try {
         const oboToken = await hentOnBehalfOfToken(accessToken, apiScope);
         const adGrupperResponse = await fetch(memberOfApiUrl, {
             headers: {
                 Authorization: `Bearer ${oboToken.access_token}`,
-                ConsistencyLevel: "eventual"
+                ConsistencyLevel: 'eventual',
             },
         });
 
         const adGrupper: MemberOfResponse = await adGrupperResponse.json();
-        return adGrupper.value.map((gruppe) => gruppe.displayName);
+        return adGrupper.value.map((gruppe) => gruppe.id);
     } catch (error) {
         const feilmelding = 'Feil under henting av brukers AD-grupper';
 

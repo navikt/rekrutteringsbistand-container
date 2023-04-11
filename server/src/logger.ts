3dd6d249e9a1e66dec34a5efa7f11488f23fb957
vildestefferud <qwartz.ms@hotmail.com>
2023-04-11 10:58:05 +0200

    Legg til secureLog

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,6 +1,16 @@
 import winston from 'winston';
+import fs from 'fs';
+
+const secureLogPath = () =>
+    fs.existsSync('/secure-logs/') ? '/secure-logs/secure.log' : './secure.log';
 
 export const logger = winston.createLogger({
     format: winston.format.json(),
     transports: new winston.transports.Console(),
+});
+
+export const secureLog = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: new winston.transports.File({ filename: secureLogPath(), maxsize: 5242880 }),
 });

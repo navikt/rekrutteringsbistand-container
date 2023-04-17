e01d46f3e1499ef596a6dbd62524aab3d567979c
vildestefferud <qwartz.ms@hotmail.com>
2023-04-17 08:54:58 +0200

    Parse json inne i body for requester

--- server/src/server.ts
+++ server/src/server.ts
@@ -50,6 +50,7 @@ const {
 
 const startServer = () => {
     app.use(compression());
+    app.use(express.json());
     app.get([`/internal/isAlive`, `/internal/isReady`], (_, res) => res.sendStatus(200));
 
     const pathsForServingApp = ['/', '/*'];

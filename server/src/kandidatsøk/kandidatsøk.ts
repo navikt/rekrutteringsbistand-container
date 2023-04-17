3e2387ffd7f03ebde24efb784329bb3f37338f3f
vildestefferud <qwartz.ms@hotmail.com>
2023-04-17 14:55:36 +0200

    Gjør funksjon async

--- server/src/kandidatsøk/kandidatsøk.ts
+++ server/src/kandidatsøk/kandidatsøk.ts
@@ -74,8 +74,8 @@ export const leggTilAuthorizationForKandidatsøkEs =
         next();
     };
 
-export const loggSøkPåFnrEllerAktørId: RequestHandler = (request, _, next) => {
-    const fnrEllerAktørId = hentFnrEllerAktørIdFraESBody(request.body);
+export const loggSøkPåFnrEllerAktørId: RequestHandler = async (request, _, next) => {
+    const fnrEllerAktørId = await hentFnrEllerAktørIdFraESBody(request.body);
 
     if (fnrEllerAktørId) {
         const brukerensAccessToken = retrieveToken(request.headers);
@@ -88,7 +88,7 @@ export const loggSøkPåFnrEllerAktørId: RequestHandler = (request, _, next) =>
     return next();
 };
 
-export const hentFnrEllerAktørIdFraESBody = (query: SearchQuery): string | null => {
+export const hentFnrEllerAktørIdFraESBody = (query: SearchQuery): Promise<string | null> => {
     let fnrEllerAktørId = null;
 
     query.query.bool?.must?.forEach((must) =>

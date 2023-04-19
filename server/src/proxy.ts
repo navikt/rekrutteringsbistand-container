317cbd763c4f93d97ecf8e705b7b176ed4fc0b00
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-04-19 08:00:15 +0200

    Test om ting fungerer nå uten logging

--- server/src/proxy.ts
+++ server/src/proxy.ts
@@ -3,7 +3,6 @@ import { respondUnauthorizedIfNotLoggedIn, tomMiddleware, setOnBehalfOfToken } f
 import {
     harTilgangTilKandidatsøk,
     leggTilAuthorizationForKandidatsøkEs,
-    loggSøkPåFnrEllerAktørId,
 } from './kandidatsøk/kandidatsøk';
 import { app } from './server';
 import { RequestHandler } from 'express';
@@ -52,7 +51,7 @@ export const proxyTilKandidatsøkEs = (
         path,
         respondUnauthorizedIfNotLoggedIn,
         harTilgangTilKandidatsøk,
-        loggSøkPåFnrEllerAktørId,
+        // loggSøkPåFnrEllerAktørId,
         leggTilAuthorizationForKandidatsøkEs(brukernavn, passord),
         setupProxy(path, proxyUrl + '/veilederkandidat_current/_search')
     );
